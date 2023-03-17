4943d84b4072a0824a87200b1929ca34cf224022
Thielison Alves <67246254+thielison@users.noreply.github.com>
2023-03-17 16:04:47 -0300

    Add setDivHoverColor() and mouse event listener to flex items
    The new setDivHoverColor() function enhances the behavior of flex items
    within the container. When the "mouseenter" event is triggered, each div
    now changes its background color, adding an interactive and visually
    pleasing touch to the layout.

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const container = document.getElementById("container");
+let flexItems;
+
+makeRows(16, 16);
+
+flexItems.forEach((item) => {
+    item.addEventListener("mouseenter", setDivHoverColor);
+});
 
 function makeRows(rows, cols) {
     for (let i = 0; i < rows * cols; i++) {
         let cell = document.createElement("div");
-        cell.className = "grid-item";
+        cell.className = "flex-item";
         container.appendChild(cell);
     }
+
+    flexItems = document.querySelectorAll(".flex-item");
 }
 
-makeRows(64, 64);
+function setDivHoverColor(e) {
+    e.target.className += " hover";
+}
+
+console.log(flexItems);
