3c9ef4ba6af0a239de948ae8001d8203b673695a
Oscar Andreazza <oscarbrum09@gmail.com>
2023-03-28 22:53:35 -0300

    alteracoes oscar

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ const toggleGridLinesButton = document.getElementById("toggle-grid-lines-button"
 const colorPicker = document.getElementById("colorpicker");
 const colorModeButton = document.getElementById("color-mode-button");
 
-let flexItems;
-let userSelectedColor;
+let flexItems = [];
+let userSelectedColor = "";
+let isRainbowModeActivated = false;
+let isGridLinesActivated = false;
+let isSingleColorModeActivated = false;
+
+colorModeButton.addEventListener("click", handleClickSingleColorMode);
+rainbowModeButton.addEventListener("click", handleClickRainbowColorMode);
+toggleGridLinesButton.addEventListener("click", handleClickGridLineMode);
+rangeInput.addEventListener("input", updateGridSizeText);
+rangeInput.addEventListener("change", updateCellsSize);
+colorPicker.addEventListener("change", watchColorPicker);
 
 function makeRows(rows, cols) {
     for (let i = 0; i < rows * cols; i++) {
@@ -21,24 +31,12 @@ function makeRows(rows, cols) {
     setDivHoverColor();
 }
 
-function setDivHoverColor() {
-    userSelectedColor = "#000";
-    flexItems.forEach((item) => {
-        item.addEventListener("mouseenter", (e) => {
+function setDivHoverColor(color) {
+    userSelectedColor = color;
+
+    flexItems.forEach(item => {
+        item.addEventListener("mouseenter", e => {
             e.target.style.backgroundColor = userSelectedColor;
-        });
-    });
-}
-
-function setRandomDivHoverColor() {
-    flexItems.forEach((item) => {
-        item.addEventListener("mouseenter", (e) => {
-            const r = Math.round(Math.random() * 256);
-            const g = Math.round(Math.random() * 256);
-            const b = Math.round(Math.random() * 256);
-            const rgb = `rgb(${r},${g},${b})`;
-
-            e.target.style.backgroundColor = rgb;
         });
     });
 }
@@ -49,7 +47,7 @@ function updateCellsSize() {
 
     makeRows(cellSize, cellSize);
 
-    flexItems.forEach((item) => {
+    flexItems.forEach(item => {
         item.style.border = "1px solid #000000";
         item.style.width = `calc(100% / ${cellSize})`;
         item.style.height = `calc(100% / ${cellSize})`;
@@ -58,40 +56,70 @@ function updateCellsSize() {
 
 function updateGridSizeText() {
     rangeOutput.textContent = rangeInput.value + " x " + rangeInput.value;
-}
-
-function toggleGridLines() {
-    flexItems.forEach((item) => {
-        if (item.style.border == "") {
-            item.style.border = "1px solid #000000";
-        } else {
-            item.style.border = "";
-        }
-    });
+    toggleGridLinesButton.classList.add("isActive");
 }
 
 function watchColorPicker(e) {
     userSelectedColor = e.target.value;
 }
 
-function activateSingleColorMode() {
-    flexItems.forEach((item) => {
-        item.addEventListener("mouseenter", (e) => {
-            e.target.style.backgroundColor = userSelectedColor;
+function handleClickSingleColorMode() {
+    isSingleColorModeActivated = !isSingleColorModeActivated;
+    if (isSingleColorModeActivated) {
+        colorModeButton.classList.add("isActive");
+        setDivHoverColor("#000");
+        rainbowModeButton.classList.remove("isActive-rainbow");
+        flexItems.forEach(item => {
+            item.addEventListener("mouseenter", e => {
+                e.target.style.backgroundColor = userSelectedColor;
+            });
         });
-    });
+    } else {
+        colorModeButton.classList.remove("isActive");
+        setDivHoverColor("");
+    }
+}
+
+function handleClickRainbowColorMode() {
+    isRainbowModeActivated = !isRainbowModeActivated;
+
+    if (isRainbowModeActivated) {
+        rainbowModeButton.classList.add("isActive-rainbow");
+        colorModeButton.classList.remove("isActive");
+
+        flexItems.forEach(item => {
+            item.addEventListener("mouseenter", e => {
+                const r = Math.round(Math.random() * 256);
+                const g = Math.round(Math.random() * 256);
+                const b = Math.round(Math.random() * 256);
+                const rgb = `rgb(${r},${g},${b})`;
+
+                e.target.style.backgroundColor = rgb;
+            });
+        });
+    } else {
+        rainbowModeButton.classList.remove("isActive-rainbow");
+        setDivHoverColor("");
+    }
+}
+
+function handleClickGridLineMode() {
+    isGridLinesActivated = !isGridLinesActivated;
+
+    if (isGridLinesActivated) {
+        toggleGridLinesButton.classList.add("isActive");
+
+        flexItems.forEach(item => {
+            item.style.border = "1px solid #000000";
+        });
+    } else {
+        toggleGridLinesButton.classList.remove("isActive");
+
+        flexItems.forEach(item => {
+            item.style.border = "";
+        });
+    }
 }
 
 rangeOutput.textContent = rangeInput.value + " x " + rangeInput.value;
-
-rangeInput.addEventListener("input", updateGridSizeText);
-rangeInput.addEventListener("change", updateCellsSize);
-
-rainbowModeButton.addEventListener("click", setRandomDivHoverColor);
-
-toggleGridLinesButton.addEventListener("click", toggleGridLines);
-
-colorPicker.addEventListener("change", watchColorPicker);
-colorModeButton.addEventListener("click", activateSingleColorMode);
-
 makeRows(16, 16); // Initialize grid with 16 x 16 size
